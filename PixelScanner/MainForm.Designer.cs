6414442afe4ad1ba69331814f6976c50f7d89b3e
Vladimir Baldetskiy <pepelaz@sibmail.com>
2015-04-14 20:52:53 +0600

    no message

---
+++ PixelScanner/MainForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace PixelScanner
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.ofd1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbImage
+            // 
+            this.pbImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbImage.Location = new System.Drawing.Point(34, 27);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(841, 511);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnProcess.Location = new System.Drawing.Point(391, 568);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 1;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 603);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.pbImage);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Main form";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.OpenFileDialog ofd1;
+    }
+}
+
---
+++ PixelScanner/MainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PixelScanner
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if (ofd1.ShowDialog() == DialogResult.OK && ofd1.FileName != "")
+            {
+                //tbAssembly.Text = dlgAssembly.FileName;
+                pbImage.Load(ofd1.FileName);
+                Bitmap bmp = new Bitmap(pbImage.Image);
+
+                uint [][]scan = new uint[bmp.Width][];
+                for (int i = 0; i < bmp.Width; i++)
+                {
+                    scan[i] = new uint[bmp.Height];
+                    for (int j = 0; j < bmp.Height; j++)
+                    {
+                        scan[i][j] = (uint)bmp.GetPixel(i, j).ToArgb();
+                    }                    
+                }
+
+                using (StreamWriter outfile = new StreamWriter("output.txt"))
+                {
+                    //for (
+                    for (int i = 0; i < bmp.Width; i++)
+                    {
+                        for (int j = 0; j < bmp.Height; j++)
+                        {
+                            outfile.Write(i);
+                            outfile.Write(" ");
+                            outfile.Write(j);
+                            outfile.Write(" ");
+                            outfile.WriteLine(scan[i][j]);
+                            }
+                    }
+                }
+                MessageBox.Show("Completed. Check output.txt file");
+            }
+        }
+    }
+}
